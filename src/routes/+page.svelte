c6e221f860dd0bc2d7f791490885dd01e1b92fea
faisalill <mfaisal.ilyas.d@gmail.com>
2024-01-14 22:54:22 +0530

    Add FPV Camera

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,12 +25,22 @@ import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 
+let canvasMounted = false;
+
+let keys = {
+  w: false,
+  a: false,
+  s: false,
+  d: false
+}
+var pointerControls;
 
 onMount(()=>{
-
+canvasMounted = true;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 65, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("canvas"), antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -38,11 +48,62 @@ renderer.physicallyCorrectLights = true
 document.body.appendChild( renderer.domElement );
 
 // const controls = new OrbitControls( camera, renderer.domElement );
-const pointerControls = new PointerLockControls( camera, renderer.domElement );
-document.addEventListener( 'click', function () {
-  pointerControls.lock();
-})
+ let fpvControls = new FirstPersonControls( camera, renderer.domElement );
+fpvControls.heightMax = 0.001;
+fpvControls.heightMin = 0;
+fpvControls.lookSpeed = 0.003;
+fpvControls.movementSpeed = 0.5;
+fpvControls.constrainVertical = true;
+fpvControls.verticalMin = 1.0;
+fpvControls.verticalMax = 2.0;
+pointerControls = new PointerLockControls( camera, renderer.domElement );
+// document.addEventListener( 'click', function () {
+//   pointerControls.lock();
+// })
 pointerControls.pointerSpeed = 2;
+
+document.addEventListener( 'keydown', function ( event ) {
+  switch ( event.keyCode ) {
+    case 38: // up
+    case 87: // w
+      keys.w = true;
+      break;
+    case 37: // left
+    case 65: // a
+      keys.a = true;
+      break;
+    case 40: // down
+    case 83: // s
+      keys.s = true;
+      break;
+    case 39: // right
+    case 68: // d
+      keys.d = true;
+      break;
+  }
+}, false );
+
+document.addEventListener( 'keyup', function ( event ) {
+  switch ( event.keyCode ) {
+    case 38: // up
+    case 87: // w
+      keys.w = false;
+      break;
+    case 37: // left
+    case 65: // a
+      keys.a = false;
+      break;
+    case 40: // down
+    case 83: // s
+      keys.s = false;
+      break;
+    case 39: // right
+    case 68: // d
+      keys.d = false;
+      break;
+  }
+}, false );
+
 // controls.enableZoom = false;
 // controls.enablePan = true;
 // controls.minDistance = 0.1;
@@ -58,22 +119,6 @@ controlTrack.zoomSpeed = 0.8;
 
 let cameraPosition = new THREE.Vector3( 5, 2, 0 );
 
-document.addEventListener( 'keydown', function ( event ) {
-// make sure to prevent anti ghosting too
-const key = event.key;
-if ( key === 'w' ) {
-  pointerControls.moveForward( 0.2 );
-  }
-if ( key === 's' ) {
-  pointerControls.moveForward( -0.2 );
-  }
-if ( key === 'a' ) {
-  pointerControls.moveRight( -0.2 );
-  }
-if ( key === 'd' ) {
-  pointerControls.moveRight( 0.2 );
-  }
-});
 
 camera.position.set( cameraPosition.x, cameraPosition.y, cameraPosition.z );
 // controls.update();
@@ -102,6 +147,7 @@ function animate() {
   // controls.update();
   // controlTrack.target.set( target.x, target.y, target.z );
   // controlTrack.update();
+    fpvControls.update(0.1);
 	renderer.render( scene, camera );
 }
 
@@ -111,6 +157,24 @@ if ( WebGL.isWebGLAvailable() ) {
   alert( 'WebGL is not available.');
 }
 })
+
+$: {
+  if(canvasMounted){
+
+  if(keys.w){
+    pointerControls.moveForward(0.1);
+  }
+  if(keys.a){
+    pointerControls.moveRight(-0.1);
+  }
+  if(keys.s){
+    pointerControls.moveForward(-0.1);
+  }
+  if(keys.d){
+    pointerControls.moveRight(0.1);
+  };
+  }
+}
 </script>
 
 <canvas class="canvas" id="canvas"></canvas>
