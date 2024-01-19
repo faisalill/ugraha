76e79b6de00c1b9300e2ffbc497fcb3fec24d1c1
faisalill <mfaisal.ilyas.d@gmail.com>
2024-01-19 23:24:54 +0530

    Update Repo

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+const satelliteParts = {
+  solar_panel_1: ["Motor_Panel_0004_1", "Motor_Panel_0004"],
+  solar_panel_2: ["Motor_Panel_0005_1", "Motor_Panel_0005"],
+  side_panel: ["side_panel"],
+  bottom_mid_pannel: ["mid_panel-2"],
+  battery: ["Mesh011", "Mesh011_1", "Mesh011_2", "Mesh011_3", "Mesh011_4", "Mesh001_5"],
+  mid_mid_panel: ["thin_panel_2"],
+  magnetometer: ["Magnetometer001", "Magnetometer002"],
+  top_mid_panel: ["thin_panel_1"],
+  payload: ["Payload001"]
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -151,6 +151,18 @@ let keys = {
   rightClick: false
 }
 
+const satelliteParts = {
+  solar_panel_1: ["Motor_Panel_0004_1", "Motor_Panel_0004"],
+  solar_panel_2: ["Motor_Panel_0005_1", "Motor_Panel_0005"],
+  side_panel: ["side_panel"],
+  bottom_mid_pannel: ["mid_panel-2"],
+  battery: ["Mesh011", "Mesh011_1", "Mesh011_2", "Mesh011_3", "Mesh011_4", "Mesh001_5"],
+  mid_mid_panel: ["thin_panel_2"],
+  magnetometer: ["Magnetometer001", "Magnetometer002"],
+  top_mid_panel: ["thin_panel_1"],
+  payload: ["Payload001"]
+}
+
 let pointerControls;
 let mixer;
 let clips;
@@ -161,7 +173,9 @@ let objects = {
 }
 
 onMount(()=>{
+
 canvasMounted = true;
+
 const scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
